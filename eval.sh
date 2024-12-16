7129156a86efb8edce768798d811ed870c5c8160
unknown <arjunbalamwar@gmail.com>
2024-12-15 21:27:58 -0800

    USe relative paths

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python eval.py \
-    --data_root 'C:/Users/ABMAN23_ML/Desktop/Joohan/USC/' \
+    --data_root 'data/' \
     --network 'pmnet_v3' \
-    --model_to_eval 'C:/Users/ABMAN23_ML/Desktop/Joohan/PMNet_Extension_Result/config_USC_pmnetV3_V2_epoch3/16_0.0001_0.5_10/model_0.03647.pt' \
+    --model_to_eval 'training_results/config_USC_pmnetV3_V2_epoch200/16_0.0001_0.5_10/model_0.00015.pt' \
     --config 'config_USC_pmnetV3_V2'
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train.py -d 'C:/Users/ABMAN23_ML/Desktop/Joohan/USC/' -n 'pmnet_v3' -c 'config_USC_pmnetV3_V2'
+python train.py -d 'data/' -n 'pmnet_v3' -c 'config_USC_pmnetV3_V2'
